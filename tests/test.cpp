2b776d3d6a5d4dba67761374d727f1b8d8946086
CodeMarco05 <brandt.marco05@gmail.com>
2025-11-03 15:26:50 +0100

    testing tries

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,14 @@
 #include "gtest/gtest.h"
+#include <iostream>
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
+  {
+    testing::internal::CaptureStdout();
+    std::cout << "Hello, World!" << std::endl;
+    testing::internal::GetCapturedStdout();
+  }
+  
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
