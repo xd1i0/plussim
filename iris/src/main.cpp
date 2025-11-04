3f48cf72f6d3946da66d4219b2a9cfe78f386759
CodeMarco05 <brandt.marco05@gmail.com>
2025-11-04 11:39:07 +0100

    sample imgui implementation

--- iris/src/main.cpp
+++ iris/src/main.cpp
@@ -1,52 +1,67 @@
-#include "raylib.h"
 #include "double_pendulum.h"
-#include <string>
-#include "string"
-#include "stellaris/models/planetes.h"
+#include "imgui.h"
+#include "raylib.h"
 #include "rlImGui.h"
+#include "stellaris/models/planetes.h"
+#include "string"
+#include <string>
 
 int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    
-    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "Double Pendulum Simulation");
+  const int screenWidth = 800;
+  const int screenHeight = 600;
 
-    SetTargetFPS(60);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  InitWindow(screenWidth, screenHeight, "Double Pendulum Simulation");
 
-    DoublePendulum pendulum(100.0f, 100.0f, 10.0f, 10.0f, M_PI / 2.0f, M_PI / 2.0f, 0.05f);
+  SetTargetFPS(60);
 
-    Vector2 origin = {screenWidth / 2.0f, 200.0f};
+  // Setup ImGui with dark theme
+  rlImGuiSetup(true);
+  float radius = 50.0f;
 
-    while (!WindowShouldClose()) {
-        pendulum.update();
+  DoublePendulum pendulum(100.0f, 100.0f, 10.0f, 10.0f, M_PI / 2.0f,
+                          M_PI / 2.0f, 0.05f);
 
-        // Get positions
-        Vector2 pos1 = pendulum.getPos1(origin);
-        Vector2 pos2 = pendulum.getPos2(origin);
+  Vector2 origin = {screenWidth / 2.0f, 200.0f};
 
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+  while (!WindowShouldClose()) {
+    pendulum.update();
 
-        DrawLineEx(origin, pos1, 2, DARKGRAY);
-        DrawLineEx(pos1, pos2, 2, DARKGRAY);
+    // Get positions
+    Vector2 pos1 = pendulum.getPos1(origin);
+    Vector2 pos2 = pendulum.getPos2(origin);
 
-        DrawCircleV(pos1, pendulum.getMass1(), RED);
-        DrawCircleV(pos2, pendulum.getMass2(), BLUE);
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
 
-        DrawCircleV(origin, 5, BLACK);
+    DrawLineEx(origin, pos1, 2, DARKGRAY);
+    DrawLineEx(pos1, pos2, 2, DARKGRAY);
 
-        int fps = GetFPS();
-        float frameTime = GetFrameTime();
-        std::string frameTimeStr = "FPS: " + std::to_string(fps) + " FrameTime: " + std::to_string(frameTime);
-        DrawText(frameTimeStr.c_str() , 10, 10, 20, BLACK);
+    DrawCircleV(pos1, pendulum.getMass1(), RED);
+    DrawCircleV(pos2, pendulum.getMass2(), BLUE);
 
-        DrawText("Double Pendulum Simulation", 10, 40, 20, BLACK);
-        DrawText("Press ESC to exit", 10, 60, 16, BLACK);
+    DrawCircleV(origin, 5, BLACK);
 
-        EndDrawing();
-    }
+    int fps = GetFPS();
+    float frameTime = GetFrameTime();
+    std::string frameTimeStr = "FPS: " + std::to_string(fps) +
+                               " FrameTime: " + std::to_string(frameTime);
+    DrawText(frameTimeStr.c_str(), 10, 10, 20, BLACK);
 
-    CloseWindow();
-    return 0;
+    DrawText("Double Pendulum Simulation", 10, 40, 20, BLACK);
+    DrawText("Press ESC to exit", 10, 60, 16, BLACK);
+
+    rlImGuiBegin();
+
+    ImGui::Begin("Controls");
+    ImGui::Text("Hello from ImGui!");
+    ImGui::SliderFloat("Circle Size", &radius, 10.0f, 200.0f);
+    ImGui::End();
+    rlImGuiEnd();
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
